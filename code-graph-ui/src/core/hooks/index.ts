b540bfcd9b856e4e8bb257028866153447821be4
pengrenchang <1096429536@qq.com>
2026-03-28 18:14:11 +0800

    feat: 添加 useChatHistory hook 管理对话历史

--- code-graph-ui/src/core/hooks/index.ts
+++ code-graph-ui/src/core/hooks/index.ts
@@ -2,7 +2,9 @@
 export { useAsync } from './useAsync'
 export { useDebounce } from './useDebounce'
 export { useAnalysisStream } from './useAnalysisStream'
+export { useChatHistory } from './useChatHistory'
 
 // Re-export types
 export type { AsyncState, AsyncActions, UseAsyncReturn } from './useAsync'
 export type { AnalysisStreamResult } from './useAnalysisStream'
+export type { UseChatHistoryReturn } from './useChatHistory'
